7155110ac02305526ce427b263ae850a118e0667
allanmagalhaes <109760879+allanmagalhaes@users.noreply.github.com>
2022-12-14 16:45:42 -0300

    Tasks todas com sucesso
    A da planilha compradores está como comentada porque só não funciona devido a um erro de nome de coluna que derma no sharepoint. Tão logo seja consertada, o comentário será removido e incorporado ao script.

--- teste_dag_suprimentos.py
+++ teste_dag_suprimentos.py
@@ -2,31 +2,193 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
-import pandas
+import pandas as pd
 
 from commons.msgraph.sharepoint.site import consultar_site
 from commons.msgraph.sharepoint.drive import selecionar_drive
+from google.oauth2 import service_account
+from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
 
 # Task 1.1 - Define DAG arguments
 default_args = {
-    'owner':'Allan',
-    'start_date':days_ago(0),
-    'email':['allan.sousa@edglobo.com.br'],
-    'email_on_failure':False,
-    'email_on_retry':False,
-    'retries':1,
-    'retry_delay':timedelta(days = 1)
+    'owner': 'Allan',
+    'start_date': days_ago(0),
+    'email': ['allan.sousa@edglobo.com.br'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(days=1)
 }
+
+# Funcao que puxa as planilhas do datapool via commons
 
 def helloworld(site, arquivo):
     site = consultar_site(site)
     drive = selecionar_drive(site_id=site.id, nome='Documentos')
     arquivo = drive.arquivo(arquivo)
 
-    excel = pandas.ExcelFile(arquivo.conteudo_binario)
-    dataframe = pandas.read_excel(excel)
+    excel = pd.ExcelFile(arquivo.conteudo_binario)
+    dataframe = pd.read_excel(excel)
 
     print(dataframe.info())
+    
+    return dataframe
+
+
+#CONEXÃO COM O BANCO DE DADOS DO PROTHEUS
+sqlserver = MsSqlHook(mssql_conn_id='edg_infobiprd_ods')
+
+
+def inserir_ods_userservices1():
+
+    
+    df_compras_radio = sqlserver.get_pandas_df(
+        sql="""SELECT [CodEmpresa]
+        ,[CodFilial]
+        ,[NumPedidoCompra]
+        ,[CodItem]
+        ,[DescAplicacao]
+        ,cast([DtEmissao] as varchar) as DtEmissao
+        ,[CodFornecedor]
+        ,[CodLoja]
+        ,[NomeFornecedor]
+        ,[CodProduto]
+        ,[DesProduto]
+        ,[DesComplementar]
+        ,[DesGrProd]
+        ,[Quantidade]
+        ,[PrcUnitario]
+        ,[VlrTotal]
+        ,[QtdEntregue]
+        ,[SaldoReceber]
+        ,[VlrSaldoRec]
+        ,[JustificativaCompra]
+        ,[CtaContabil]
+        ,[DesConta]
+        ,[CodCentroCusto]
+        ,[DesCentroCusto]
+        ,[ClasseValor]
+        ,[DesClasVlr]
+        ,[PedEncerrado]
+        ,[CodUsuario]
+    FROM [ODS].[dbo].[tbODSPedidosComprasRadio]"""
+    )
+    return df_compras_radio
+
+def inserir_ods_userservices2():
+    
+    df_fato_requisicao = sqlserver.get_pandas_df(
+        sql= """SELECT [Num_requisicao]
+        ,[Item]
+        ,[DataSolicitacao]
+        ,[DataModificacao]
+        ,[Criadopor]
+        ,[CodMaterial]
+        ,[TextoBreve]
+        ,[Quantidade]
+        ,[UnidadeMedida]
+        ,[DataRemessa]
+        ,[GrupoMercadorias]
+        ,[Centro]
+        ,[dataPedido]
+        ,[GrpCompradores]
+        ,[NumPedido]
+        ,[TemPedido]
+        ,[ItemPedido]
+        ,[SlaAtendido]
+        ,[CentroCusto]
+        ,[TipoDocumento]
+        ,[CategoriaItem]
+        ,[CodEliminação] as CodEliminacao
+    FROM [FINANC].[dbo].[vwCompras_FatoRequisicao]"""
+    )
+    return df_fato_requisicao
+
+def inserir_ods_userservices3():
+    
+    df_conexao_fato_pedido = sqlserver.get_pandas_df(
+        sql= """SELECT [NumPedido]
+        ,[ItemPedido]
+        ,[PedidoItem]
+        ,[DataCriacao]
+        ,[DataModificacao]
+        ,[IdMaterial]
+        ,[Empresa]
+        ,[Quantidade]
+        ,[PrecoLiquidoMoedaDoc]
+        ,[ValorLiquido]
+        ,[ValorBruto]
+        ,[CodIVA]
+        ,[CodRecusa]
+        ,[FlagEntradaMercadoria]
+        ,[FlagEntradaMercadoriaNaoAvaliada]
+        ,[NumConfirmacaoOrdem]
+        ,[NumContrato]
+        ,[NumItemContrato]
+        ,[CodUnidadeMedida]
+        ,[ValorContrato]
+        ,[ValorIvaNaoDedutivel]
+        ,[QtdSolicitadaNormal]
+        ,[DataDeterminacaoPreco]
+        ,[ValorEfetivoItem]
+        ,[CodCliente]
+        ,[NumEnderecoDocCompra]
+        ,[DiasPrazoEntrega]
+        ,[PesoLiquido]
+        ,[CodUnidadePeso]
+        ,[CodDomicilioFiscal]
+        ,[PesoBruto]
+        ,[Volume]
+        ,[CodTipoMaterial]
+        ,[CodTipoDocCompras]
+        ,[StatusDocCompras]
+        ,[NomeResponsavel]
+        ,[IntervaloItens]
+        ,[UltimoNumItem]
+        ,[IdFornecedor]
+        ,[ChaveCondicoesPagamento]
+        ,[CodOrganizacaoCompras]
+        ,[CodGrupoCompradores]
+        ,[IdMoeda]
+        ,[DataDocCompra]
+        ,[Cancelado]
+        ,[TaxaCambio]
+        ,[IdSituacao]
+    FROM [FINANC].[dbo].[vwCompras_FatoPedido]"""
+    )
+    return df_conexao_fato_pedido
+
+def joganoGCP():
+    df_criado_por = helloworld('/sites/PaineldeGestao', '/Compras/POWER BI/CRIADO POR.xlsx')
+    #df_gr_compradores = helloworld('/sites/PaineldeGestao', '/Compras/POWER BI/GRUPO COMPRADORES.xlsx')
+    df_protheus_de_para = helloworld('/sites/PaineldeGestao', '/Compras/POWER BI/PROTHEUS_DE PARA.xlsx')
+    df_fun_mes = helloworld('/sites/PaineldeGestao', '/Compras/POWER BI/Relação Func_Mês.xlsx')
+    df_saving_contratos = helloworld('/sites/PaineldeGestao', '/Compras/POWER BI/Saving_Contratos.xlsx')
+
+    #TRATAMENTO DOS DADOS
+    #df_gr_compradores.rename(columns={'Categoria ':'Categoria', 'Nome ':'Nome','Célula':'Celula'}, inplace = True)
+    df_protheus_de_para.rename(columns={'Cod. Usuario':'CodUsuario', 'Nome Usuario':'NomeUsuario'}, inplace = True)
+    df_fun_mes.rename(columns={'CÉLULA':'CELULA', 'Quant. Analista':'QuantAnalista'}, inplace = True)
+    df_saving_contratos.rename(columns={'Nome_Sav Cont':'NomeSavCont'}, inplace = True)
+
+
+
+    #CONEXÃO CLOUD BIGQUERY
+    import json
+
+    key_path = "/opt/airflow/chaves/edg-infra-bi.json"
+    credentials = service_account.Credentials.from_service_account_file(key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"])
+
+
+    #CARGA DOS DATAFRAMES NO BIGQUERY
+    inserir_ods_userservices1().to_gbq(credentials=credentials, destination_table = 'Compras.tb_Pedidos_Compras', if_exists='replace')
+    inserir_ods_userservices2().to_gbq(credentials=credentials, destination_table = 'Compras.tb_Fato_Requisicao', if_exists='replace')
+    inserir_ods_userservices3().to_gbq(credentials=credentials, destination_table = 'Compras.tb_Fato_Pedido', if_exists='replace')
+    df_criado_por.to_gbq(credentials=credentials, destination_table = 'Compras.tb_criado_por', if_exists='replace')
+    #df_gr_compradores.to_gbq(credentials=credentials, destination_table = 'Compras.tb_gr_compradores', if_exists='replace')
+    df_protheus_de_para.to_gbq(credentials=credentials, destination_table = 'Compras.tb_protheus_de_para', if_exists='replace')
+    df_fun_mes.to_gbq(credentials=credentials, destination_table = 'Compras.tb_fun_mes', if_exists='replace')
+    df_saving_contratos.to_gbq(credentials=credentials, destination_table = 'Compras.tb_saving_contratos', if_exists='replace')
 
 with DAG(
     dag_id='teste_dag_suprimentos',
@@ -36,13 +198,23 @@ with DAG(
 
 ) as dag:
     datapool1 = PythonOperator(
-    task_id = 'extrair_datapool_1',
-    python_callable=helloworld,
-    op_kwargs={
-        'site': '/sites/PaineldeGestao',
-        'arquivo': '/Compras/POWER BI/CRIADO POR.xlsx'
-    }
-)
+        task_id='extrair_datapool_1',
+        python_callable=helloworld,
+        op_kwargs={
+            'site': '/sites/PaineldeGestao',
+            'arquivo': '/Compras/POWER BI/CRIADO POR.xlsx'
+        }
+    )
+
+#datapool2 = PythonOperator(
+#    dag = dag,
+#    task_id='extrair_datapool_2',
+#    python_callable=helloworld,
+#    op_kwargs={
+#        'site': '/sites/PaineldeGestao',
+#        'arquivo': '/Compras/POWER BI/GRUPO COMPRADORES.xlsx'
+#    }
+#)
 
 datapool2 = PythonOperator(
     dag = dag,
@@ -50,7 +222,7 @@ datapool2 = PythonOperator(
     python_callable=helloworld,
     op_kwargs={
         'site': '/sites/PaineldeGestao',
-        'arquivo': '/Compras/POWER BI/GRUPO COMPRADORES.xlsx'
+        'arquivo': '/Compras/POWER BI/PROTHEUS_DE PARA.xlsx'
     }
 )
 
@@ -60,7 +232,7 @@ datapool3 = PythonOperator(
     python_callable=helloworld,
     op_kwargs={
         'site': '/sites/PaineldeGestao',
-        'arquivo': '/Compras/POWER BI/PROTHEUS_DE PARA.xlsx'
+        'arquivo': '/Compras/POWER BI/Relação Func_Mês.xlsx'
     }
 )
 
@@ -70,28 +242,33 @@ datapool4 = PythonOperator(
     python_callable=helloworld,
     op_kwargs={
         'site': '/sites/PaineldeGestao',
-        'arquivo': '/Compras/POWER BI/Relação Func_Mês.xlsx'
-    }
-)
-
-datapool5 = PythonOperator(
-    dag = dag,
-    task_id='extrair_datapool_5',
-    python_callable=helloworld,
-    op_kwargs={
-        'site': '/sites/PaineldeGestao',
-        'arquivo': '/Compras/POWER BI/SAVING.xlsx'
-    }
-)
-
-datapool6 = PythonOperator(
-    dag = dag,
-    task_id='extrair_datapool_6',
-    python_callable=helloworld,
-    op_kwargs={
-        'site': '/sites/PaineldeGestao',
         'arquivo': '/Compras/POWER BI/Saving_Contratos.xlsx'
     }
 )
 
-datapool1 >> datapool2 >> datapool3 >> datapool4 >> datapool5 >> datapool6
+inserirODS = PythonOperator(
+    dag = dag,
+    task_id='inserir_dados_ods',
+    python_callable=inserir_ods_userservices1
+)
+
+inserirODS2 = PythonOperator(
+    dag = dag,
+    task_id='inserir_dados_ods2',
+    python_callable=inserir_ods_userservices2
+)
+
+inserirODS3 = PythonOperator(
+    dag = dag,
+    task_id='inserir_dados_ods3',
+    python_callable=inserir_ods_userservices3
+)
+
+
+alimentaBQ = PythonOperator(
+    dag = dag,
+    task_id='alimentaBQ',
+    python_callable=joganoGCP
+)
+
+datapool1 >> datapool2 >> datapool3 >> datapool4 >> inserirODS >> inserirODS2 >> inserirODS3 >> alimentaBQ
